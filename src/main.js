61ec2df78576d26bf960fa455f686b90a6baecb2
noook <me@neilrichter.com>
2021-01-06 21:39:08 +0100

    Installing composition API with vue-demi

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import DesignSystem from 'design-system';
+import { install } from 'vue-demi';
 
+// The library will actually vue-demi so that's
+// how I need to install the composition API
+install(Vue);
 Vue.use(DesignSystem);
-// Not installing Vue composition, I already know it's installed in the package
-// I know it's bad practice, but it's just for the reproduction case
 Vue.config.productionTip = false
 
 new Vue({
